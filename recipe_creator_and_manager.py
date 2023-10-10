10457e928c76faec8c2b1031788b095bf8edf35e
Swetha-Uppula <144287740+Swetha-Uppula@users.noreply.github.com>
2023-10-10 15:33:37 -0600

    Create recipe_creator_and_manager.py

---
+++ recipe_creator_and_manager.py
@@ -0,0 +1,94 @@
+import csv
+
+class Recipe:
+    def __init__(self, name, ingredients, category):
+        self.name = name
+        self.ingredients = ingredients
+        self.category = category
+
+class RecipeManager:
+    def __init__(self):
+        self.recipes = []
+
+    def add_recipe(self, recipe):
+        self.recipes.append(recipe)
+
+    def get_recipes_by_category(self, category):
+        return [recipe for recipe in self.recipes if recipe.category == category]
+
+    def get_recipes_by_ingredient(self, ingredient):
+        return [recipe for recipe in self.recipes if ingredient in recipe.ingredients]
+
+def create_recipe():
+    name = input("Enter recipe name: ")
+    ingredients = input("Enter ingredients (comma-separated): ").split(',')
+    category = input("Enter category: ")
+    return Recipe(name, ingredients, category)
+
+def save_recipes_to_csv(recipes):
+    with open('recipes.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Recipe Name", "Category", "Ingredients"])
+
+        for recipe in recipes:
+            writer.writerow([recipe.name, recipe.category, ", ".join(recipe.ingredients)])
+
+def load_recipes_from_csv():
+    recipes = []
+    try:
+        with open('recipes.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['Recipe Name']
+                category = row['Category']
+                ingredients = [ingredient.strip() for ingredient in row['Ingredients'].split(',')]
+                recipes.append(Recipe(name, ingredients, category))
+    except FileNotFoundError:
+        pass  # Handle the case where the file doesn't exist
+    return recipes
+
+def main():
+    recipe_manager = RecipeManager()
+    recipes = load_recipes_from_csv()
+    recipe_manager.recipes = recipes
+
+    while True:
+        print("\nRecipe Manager Menu:")
+        print("1. Create a recipe")
+        print("2. View recipes by category")
+        print("3. View recipes by ingredient")
+        print("4. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            recipe = create_recipe()
+            recipe_manager.add_recipe(recipe)
+            print(f"Recipe '{recipe.name}' added successfully!")
+            save_recipes_to_csv(recipe_manager.recipes)
+        elif choice == '2':
+            category = input("Enter category to view recipes: ")
+            recipes = recipe_manager.get_recipes_by_category(category)
+            if recipes:
+                print(f"Recipes in the '{category}' category:")
+                for recipe in recipes:
+                    print(recipe.name)
+            else:
+                print(f"No recipes found in the '{category}' category.")
+        elif choice == '3':
+            ingredient = input("Enter ingredient to view recipes: ")
+            recipes = recipe_manager.get_recipes_by_ingredient(ingredient)
+            if recipes:
+                print(f"Recipes containing '{ingredient}':")
+                for recipe in recipes:
+                    print(recipe.name)
+            else:
+                print(f"No recipes found containing '{ingredient}'.")
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
